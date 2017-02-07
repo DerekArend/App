d45c23ec215807fd4c78df6830f06c8c81434353
DerekArend <derekaorm@gmail.com>
2017-02-07 14:56:06 -0900

    Update ricky.java

--- ricky.java
+++ ricky.java
@@ -1,1 +1,27 @@
-somewhere
+
+public class Application {
+
+	public static void main(String[] args) {
+		int myNumber = 88;
+		long myLong = 9797;
+		short myShort = 847;
+		double myDouble = 7.3243;
+		float myFloat = 324.3f;
+		
+		char myChar = 'g';
+		boolean myBoolean = true;
+		
+		byte myByte = 127;
+		
+		System.out.println(myByte);
+		System.out.println(myBoolean);		
+		System.out.println(myChar);
+		System.out.println(myFloat);
+		System.out.println(myDouble);
+		System.out.println(myShort);
+		System.out.println(myNumber);
+		System.out.println(myLong);
+		
+	}
+
+}
